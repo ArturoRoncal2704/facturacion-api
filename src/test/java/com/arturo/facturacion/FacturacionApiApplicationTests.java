259b59fb9fafe6127d6ae97fdb4a6348be2925bf
Arturo Roncal <roncalhermenegildo@gmail.com>
2026-02-20 01:42:26 -0500

    Initial commit

---
+++ src/test/java/com/arturo/facturacion/FacturacionApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arturo.facturacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacturacionApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
