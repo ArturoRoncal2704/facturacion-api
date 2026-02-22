313706c88c0b3dc9d161ace7123c4cb960f6b7a9
Arturo Roncal <roncalhermenegildo@gmail.com>
2026-02-21 20:46:34 -0500

    feat: implementar CRUD para Servicios

--- src/main/java/com/arturo/facturacion/controller/ClienteController.java
+++ src/main/java/com/arturo/facturacion/controller/ClienteController.java
@@ -4,7 +4,6 @@ import com.arturo.facturacion.dto.ClienteRequest;
 import com.arturo.facturacion.entity.Cliente;
 import com.arturo.facturacion.service.ClienteService;
 import lombok.RequiredArgsConstructor;
-import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
